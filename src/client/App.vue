f6669d7882629bf02adee5dd5f84f34df5a63632
mkosmala <mac.kosmala@gmail.com>
2026-05-30 00:34:54 +0200

    chore: configure tailwind + scss

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,3 +1,5 @@
 <template>
-  <div />
+  <div class="flex min-h-screen items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600">Tailwind works!</h1>
+  </div>
 </template>
