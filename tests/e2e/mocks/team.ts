40745cffabe28384ac5c10de8cdd6d6f431df715
mkosmala <mac.kosmala@gmail.com>
2026-06-01 01:13:06 +0200

    test: isolate e2e test with api mocking

---
+++ tests/e2e/mocks/team.ts
@@ -0,0 +1,23 @@
+export const mockArsenal = {
+  teams: [
+    {
+      idTeam: '133604',
+      strTeam: 'Arsenal',
+      strLeague: 'English Premier League',
+      strCity: 'London',
+      strCountry: 'England',
+      intFormedYear: '1886',
+      strDescriptionEN:
+        'Arsenal Football Club is a professional football club based in Islington, London.',
+      strStadium: 'Emirates Stadium',
+      strStadiumThumb: null,
+      intStadiumCapacity: '60355',
+      strWebsite: 'www.arsenal.com',
+      strTwitter: 'twitter.com/Arsenal',
+      strFacebook: null,
+      strInstagram: null,
+      strYoutube: null,
+      strBadge: null,
+    },
+  ],
+};
---
+++ tests/e2e/mocks/teams.ts
@@ -0,0 +1,3 @@
+export const mockTeams = {
+  teams: [{ idTeam: '133604', strTeam: 'Arsenal', strBadge: null }],
+};
